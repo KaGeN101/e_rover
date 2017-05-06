1d616c61802849f4f5b56ea6032989eaf5387704
Johann Eloff <johanne@urup.com>
2017-05-06 22:32:31 +0200

    Fixing parsing test

--- test/instruction_test.exs
+++ test/instruction_test.exs
@@ -3,7 +3,7 @@ defmodule InstructionTest do
   import ERover.Instructions, only: [ parse: 1 ]
 
   test "test the data" do
-    parse ["8 8", "1 2 E", "MMLMM"] 
+    assert parse ["8 8", "1 2 E", "MMLMM"] == %{grid: %{rows: 8, cols: 8}} 
   end
 end
 
